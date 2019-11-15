56636c6ee3ec6ca23011ec3140ac7fafe69ec908
Edward Stevens <edwardstevens@Edwards-MacBook-Pro.local>
2019-11-15 17:52:50 +0700

    functionalities added comment, edit, avatar follow across site, css link,

--- app.py
+++ app.py
@@ -20,9 +20,10 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), nullable = False , unique = True)
     password = db.Column(db.String(300), nullable = False) 
     name = db.Column(db.String(100), nullable = True)
+    avatar_url = db.Column(db.String, server_default="https://cdn0.iconfinder.com/data/icons/hr-business-and-finance/100/face_human_blank_user_avatar_mannequin_dummy-512.png"
+    )
 
-
-    def set_password(self, password):
+    def generate_password(self, password):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
@@ -32,14 +33,17 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
     user_id = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now()) 
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
 
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
     user_id = db.Column(db.Integer, nullable=False)
     post_id = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now()) 
@@ -55,7 +59,41 @@ def load_user(user_id):
 
 @app.route('/') 
 def root():
-    return render_template('views/index.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    posts = Post.query.all()
+    posts = Post.query.order_by(Post.created_at.desc()).all()
+    
+    if request.args.get('filter') == 'oldest':
+        posts = Post.query.order_by(Post.created_at.asc()).all()
+        return render_template('views/index.html', posts = posts)
+    for post in posts: 
+        user = User.query.get(post.user_id)
+        post.username = user.name
+        post.avatar_url = user.avatar_url
+    return render_template('views/index.html', posts = posts)
+
+@app.route('/logout')
+@login_required  
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=[ 'GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('root'))
+    if request.method == "POST":
+        user = User.query.filter_by(email = request.form["email"]).first()
+        if not user: 
+            flash("Your email is not registered",'warning')
+            return redirect(url_for('register'))
+        if user.check_password(request.form["password"]):   
+            login_user(user)
+            flash("Welcome back Ed missed you!", "success")
+            return redirect(url_for('root'))
+        flash('Incorrect password, please try to login again', 'warning')
+    return render_template("views/login.html")
 
 @app.route('/register', methods=['POST','GET'])
 def register():
@@ -68,14 +106,75 @@ def register():
             return redirect(url_for('register')) 
         
         new_user = User(name=request.form['name'],  
-                        email=request.form['email'])
-        new_user.generate_password(request.form['password'])  # raw password will be hashed using the generate_password method
+                        email=request.form['email'],
+                        avatar_url = request.form['avatar_url']
+                        )
+        new_user.generate_password(request.form['password'])  
         db.session.add(new_user) # then we add new user to our session
         db.session.commit() # then we commit to our database (kind of like save to db)
         login_user(new_user) # then we log this user into our system
         flash('Successfully create an account and logged in', 'success')
         return redirect(url_for('root')) # and redirect user to our root
     return render_template('views/register.html') 
+
+
+@app.route('/post', methods= ['POST'])
+@login_required 
+def create_post():
+    if request.method == 'POST':
+        new_post = Post(
+            body = request.form['body'],
+            image_url = request.form['image_url'],
+            user_id = current_user.id
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        flash('Thank you for posting on edbook the smallest social network in the world', 'success')
+        return redirect(url_for('root'))
+    return redirect(url_for('root')) 
+
+@app.route('/posts/<id>')
+@login_required
+def view_post(id):
+
+    post = Post.query.get(id)
+    comments = Comment.query.filter_by(post_id = post.id).all()
+    
+    for comment in comments: 
+        user = User.query.get(comment.user_id)
+        comment.username = user.name
+        comment.avatar_url = user.avatar_url
+    
+    return render_template('views/post-view.html', post = post , comments = comments ) 
+
+@app.route('/posts/<id>/comments', methods= ['POST'])
+@login_required
+def create_comment(id):
+    if request.method == 'POST':
+        comment = Comment(
+        user_id = current_user.id , 
+        post_id = id,
+        body = request.form['body'],
+        image_url = request.form['image_url'] 
+        )
+        db.session.add(comment)
+        db.session.commit()
+        flash('Thank you for commenting on edbook the smallest social network in the world', 'success')
+        return redirect(url_for('view_post', id=id))
+
+
+
+@app.route('/posts/<id>', methods= ['POST'])
+@login_required
+def edit_post(id):
+    post = Post.query.get(id)
+    if request.method == 'POST':
+        post.body = request.form['body']
+        post.image_url = request.form['image_url']
+        db.session.commit()
+        flash('Thank you for editing your post on edbook the smallest social network in the world', 'success')
+        return redirect(url_for('view_post', id=id))   
+
 
 if __name__ == "__main__":
     app.run(debug = True)
